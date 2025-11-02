be4c853572ac573ec6bdba27c2d35b12b58b3a24
super <97927259+Supraboy981322@users.noreply.github.com>
2025-11-02 09:52:02 -0600

    code comments

--- libs.go
+++ libs.go
@@ -12,56 +12,57 @@ import (
 	"errors"
 )
 
-
+//write str with no newline
 func wr(str string) {
 	os.Stdout.WriteString(str)
 }
 
-
+//write byte to stdout
 func wrb(byt []byte) {
 	os.Stdout.Write(byt)
 }
 
-
+//write ln
 func wrl(str string) {
 	wr(str + "\n")
 }
 
-
+//just print err
 func werr(err error) {
 	os.Stderr.WriteString("" + err.Error() + "\n")
 }
 
-
+//just print err from str
 func wserr(err string) {
 	werr(errors.New(err))
 }
 
-
+//fatal err 
 func ferr(err error) {
 	werr(err)
 	os.Exit(1)
 }
 
-
+//fatal err from str
 func fserr(err string) {
 	ferr(errors.New(err))
 }
 
-
+//handle errs as non-fatal
 func hanErr(err error) {
 	if err != nil {
 		werr(err)
 	}
 }
 
-
+//handle errs as fatal
 func hanFrr(err error) {
 	if err != nil {
 		ferr(err)
 	}
 }
 
+//mk err
 func merr(str string, err error) {
 	erorStr := str + err.Error()
 	eror := errors.New(erorStr)
