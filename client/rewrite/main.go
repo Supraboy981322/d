453744c768fae3c9cef05398bf0cf56c9ac4a37a
super <97927259+Supraboy981322@users.noreply.github.com>
2025-11-02 12:41:17 -0600

    start work on rewrite to use BubbleTea (sucks that it needs a rewrite for that

---
+++ client/rewrite/main.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+//	"fmt"
+	"net/http"
+	"strings"
+	"time"
+	"os"
+	"io"
+
+	"github.com/charmbracelet/bubbles/spinner"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/BurntSushi/toml"
+)
+
+var (
+	url string   //set later when read conf
+	line string
+
+	//changes to actual path
+	confDir = "/.config/Supraboy981322/d"
+	confPath = "/config.toml"
+
+	defaultConfig = []byte(`[server]
+address = "https://example.com/"`)
+)
+
+type (
+	ServerConfig struct {
+		Addr string `toml:"address"`
+	}
+	Config struct {
+		Server ServerConfig
+	}
+
+	confMsg string
+	argsMsg string
+	respMsg string
+	errMsg struct{ error }
+
+	model struct {
+		url string
+		config *Config
+		line string
+		response string
+		spinner spinner.Model
+		quitting bool
+		err error
+	}
+)
+
+func (m model) Init() tea.Cmd {
+	return chkArgs()
+}
+
+func main() {
+	p := tea.NewProgram(model{})
+	if _, err := p.Run(); err != nil {
+		ferr(err)
+	}
+}
+
+func (e errMsg) Error() string {
+	return e.error.Error()
+}
+
+func chkArgs() tea.Cmd {
+	return func() tea.Msg {
+		//chk args
+		if len(os.Args) > 1 {
+			line = os.Args[1]
+			return argsMsg(line)
+		}
+		wserr("no input")
+		tea.Quit
+		return argsMsg(line)
+	}
+}
+
+func readConf() tea.Cmd {
+	return func() tea.Msg {
+		//get ~
+		homeDir, err := os.UserHomeDir()
+		hanFrr(err)
+	
+		//set actual conf dir and path
+		confDir = homeDir + confDir
+		confPath = confDir + confPath
+	
+		//chk if it exists
+		_, err = os.Stat(confPath)
+		if os.IsNotExist(err) {
+			//mk if not
+			hanErr(os.MkdirAll(confDir, 0755))
+			
+			//write default conf
+			hanFrr(os.WriteFile(
+				confPath, defaultConfig, 0644))
+		}
+	
+		//read conf
+		var conf Config
+		_, err = toml.DecodeFile(confPath, &conf)
+		hanFrr(err)
+		
+		//set url from conf
+		url := conf.Server.Addr
+		if url == "https://example.com/" || url == "" {
+			if e, ok := merr("server address not set", nil).(errMsg); ok {
+				return errMsg(e)
+			}
+		}
+		return confMsg(url)
+	}
+}
+
+func sendReq() tea.Cmd {
+	return func() tea.Msg {
+		//create http client
+		//  timeout after 10 seconds
+		client := &http.Client{
+			Timeout: time.Second * 10,
+		}
+	
+		//create req
+			req, err := http.NewRequest(
+			"POST", url, strings.NewReader(line))
+		if err != nil {
+			if e, ok := merr("server address not set", err).(errMsg); ok {
+				return errMsg(e)
+			}
+		}
+	
+		//set header (not needed, but just to be safe)
+		req.Header.Set("Content-Type", "text/plain")
+	
+		//mk req
+		resp, err := client.Do(req)
+		if err != nil {
+			if e, ok := merr("failed to send request", err).(errMsg); ok {
+				return errMsg(e)
+			}
+		}
+	
+		//read resp 
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			if e, ok := merr("server address not set", err).(errMsg); ok {
+				return errMsg(e)
+			}
+		}
+	
+		//return resp
+		return respMsg(string(body))
+	}
+}
---
+++ client/rewrite/ui.go
@@ -0,0 +1,77 @@
+package main
+
+import(
+	"fmt"
+
+	tea "github.com/charmbracelet/bubbletea"
+//	"github.com/charmbracelet/bubbles/spinner"
+//	"github.com/charmbracelet/lipgloss"
+)
+
+/*func initialModel() model {
+	s := spinner.New()
+	s.Spinner = spinner.Dot
+	s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+	
+}*/
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "esc", "ctrl+c":
+			m.quitting = true
+			return m, tea.Quit
+		default:
+			return m, nil
+		}
+	case argsMsg:
+		line = string(msg)
+		cmd := func() tea.Msg {
+			return readConf()()
+		}
+		return m, cmd
+	case confMsg:
+		m.url = string(msg)
+		cmd := func() tea.Msg {
+			return sendReq()()
+		}
+		return m, cmd
+	case respMsg:
+		m.response = string(msg)
+		return m, tea.Quit
+	case errMsg:
+		m.err = error(msg)
+		return m, nil
+	default:
+		var cmd tea.Cmd
+		m.spinner, cmd = m.spinner.Update(msg)
+		return m, cmd
+	}
+}
+
+func (m model) View() string {
+	if m.quitting {
+		return "quitting...\n"
+	}
+
+	out := ""
+	out += fmt.Sprintf("resp:  %s\n", m.response)
+	
+	if m.config != nil {
+		out += fmt.Sprintf("server:  %s\n", m.config.Server)
+	}
+
+	if m.err != nil {
+		out += fmt.Sprintf("err:  %v\n", m.err)
+		out += "\npress q to quit.\n"
+		return out
+	}
+
+	if m.line != "" {
+		out += "press q to quit.\n"
+		return out
+	}
+	out += "working...\n"
+	return out
+}
