db133b5f6544a2f36e8a7706141944580835f0c2
keeper of the interwebs <97927259+Supraboy981322@users.noreply.github.com>
2026-03-12 16:17:32 -0500

    sync webui board on load

--- src/dServer/web/index.js
+++ src/dServer/web/index.js
@@ -3,6 +3,8 @@ const body = dom.body;
 const domain = window.location.origin;
 
 function construct() {
+  body.innerHTML = "";
+
   let page_container = dom.createElement("div");
   body.appendChild(page_container);
   page_container.id = "page_container";
@@ -11,6 +13,10 @@ function construct() {
   page_container.appendChild(board);
   board.id = "board";
 
+  let msg_container = dom.createElement("div");
+  board.appendChild(msg_container);
+  msg_container.className = "container";
+
   let msg_box = dom.createElement("input");
   board.appendChild(msg_box);
   msg_box.className = "msg_box";
@@ -18,17 +24,20 @@ function construct() {
   msg_box.addEventListener("keydown", (event) => {
     if (event.key === "Enter") send();
   });
+
+  update_board();
 }
 construct()
 
 async function send(msg) {
+  const d = new Date();
+  let timestamp = `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
   let input = dom.querySelector("#board > input.msg_box")
   if (msg === null || msg === undefined) msg = input.value;
   input.value = "";
   console.log(msg);
-  let url = `${domain}/post`; 
   try {
-    let resp = await fetch(url, {
+    let resp = await fetch(`${domain}/post`, {
       method: "POST",
       body: msg,
     });
@@ -37,11 +46,44 @@ async function send(msg) {
   } catch (e) {
     console.error(e);
     alert(e);
+    return;
   }
+  new_msg_elem({
+    Timestamp: timestamp,
+    Msg: msg,
+  });
 }
 
-function new_msg_elm(msg) {
+async function update_board() {
+  let resp;
+  try {
+    resp = await fetch(`${domain}/today`, {
+      method: "GET",
+    });
+    if (!resp.ok) throw new Error(`SERVER ERR: ${resp}`);
+  } catch (e) {
+    console.error(e);
+    alert(e);
+    return;
+  }
+
+  let json = await resp.json();
+  json.forEach((msg) => new_msg_elem(msg));
+}
+
+function new_msg_elem(msg) {
   let msg_container = dom.createElement("div");
-  msg_container.id = msg.timestamp;
+  dom.querySelector("#board > .container").appendChild(msg_container);
+  msg_container.id = msg.Timestamp;
   msg_container.className = "msg";
+
+  let timestamp = dom.createElement("p");
+  msg_container.appendChild(timestamp);
+  timestamp.className = "timestamp";
+  timestamp.innerText = msg.Timestamp;
+
+  let msg_txt = dom.createElement("p");
+  msg_container.appendChild(msg_txt);
+  msg_txt.className = "txt";
+  msg_txt.innerText = msg.Msg;
 }
