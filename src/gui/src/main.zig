38f75d956b102ca9c09b0e97b040111af147fb23
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-04-17 15:14:11 -0500

    render text box and properly handle overflow

--- src/gui/src/main.zig
+++ src/gui/src/main.zig
@@ -46,7 +46,43 @@ pub fn main() !void {
 
         rl.beginDrawing();
         defer rl.endDrawing();
+
         rl.clearBackground(.black);
-        rl.drawText("foo", 100, 100, 20, .white);
+        rl.drawRectangleRec(txt_box, .white);
+
+        const elderly_input_buf:[:0]const u8 = b: {
+            var itr = std.mem.reverseIterator(input_txt.items);
+            var buf = try std.ArrayList(u8).initCapacity(alloc, 0);
+            defer buf.deinit(alloc);
+            var buf_elderly = try alloc.allocSentinel(u8, 0, 0);
+            while (itr.next()) |b| {
+                try buf.append(alloc, b);
+                alloc.free(buf_elderly);
+                buf_elderly = try alloc.dupeZ(u8, buf.items);
+                if (@as(f32, @floatFromInt(rl.measureText(buf_elderly, 20))) > txt_box.width) break;
+            }
+            for (0..3) |_| _ = buf.pop();
+            alloc.free(buf_elderly);
+            buf_elderly = try alloc.dupeZ(u8, buf.items);
+            std.mem.reverse(u8, buf_elderly);
+            break :b buf_elderly;
+        };
+        defer alloc.free(elderly_input_buf);
+
+        rl.drawText(
+            elderly_input_buf,
+            @intFromFloat(txt_box.x + 5),
+            @intFromFloat(txt_box.y + 8),
+            20,
+            .dark_gray,
+        );
+
+        if ((frame_count / 20) % 2 == 0) rl.drawText(
+            "_",
+            @as(i32, @intFromFloat(txt_box.x)) + 8 + rl.measureText(elderly_input_buf, 20),
+            @intFromFloat(txt_box.y + 12),
+            20,
+            .blue
+        );
     }
 }
