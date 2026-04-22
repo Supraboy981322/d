08f74ff4900bf9762be4e9307de5c95d4eb08aef
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-04-21 21:33:23 -0500

    pin Zig to 0.15.2

--- src/gui/flake.nix
+++ src/gui/flake.nix
@@ -2,14 +2,33 @@
   description = "d_gui";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    # import Zig overlay
+    zig_overlay = {
+      url = "github:mitchellh/zig-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, zig_overlay }:
     (flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        zigVersion = "0.15.2";
+
+        # selected Zig package
+        zig = zig_overlay.packages.${system}.${zigVersion};
+
+        # add the Zig overlay pkgs
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ zig_overlay.overlays.default ];
+        };
+
+        server = import ./server.nix;
       in {
         devShells.default = pkgs.mkShell ({
-          packages = with pkgs; [
+          packages = (with pkgs; [
             # raylib deps
             mesa
             libXi
@@ -19,7 +38,7 @@
             libXinerama
             wayland
             libxkbcommon
-          ];
+          ]) ++ [ zig ];
         });
       })
     );
